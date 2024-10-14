b20dc709d0bea19a90179cd1664c564e51463850
Ravisanker E <ravisanker1999@gmail.com>
2024-10-14 19:26:05 +0530

    Initialize modal-component

---
+++ blocks/form/components/modal/modal.js
@@ -0,0 +1,13 @@
+import { createModal } from '../../../modal/modal.js';
+import { subscribe } from '../../rules/index.js';
+
+export default async function decorate(panel) {
+  const modal = await createModal(panel);
+  subscribe(panel, async (fieldDiv, formModel) => {
+    modal.setFormModel(formModel);
+    if (formModel.getElement(fieldDiv.id).visible === true) {
+      modal.showModal();
+    }
+  });
+  return modal.block;
+}
